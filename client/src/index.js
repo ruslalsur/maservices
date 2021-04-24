1084504a8d72b23962d4a8c766c327ca57be5b8b
rusla <suhrugen@gmail.com>
2021-04-24 22:55:00 +0700

    DEV testing base url

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+console.log(`PUBLIC_URL: `, process.env.PUBLIC_URL)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
